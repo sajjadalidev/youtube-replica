61d4308dc6fc75986975d862062ba44f08064e2d
Sajjad Ali <iamsajjadali.dev@gmail.com>
2025-06-06 03:43:10 +0500

    Enhancement - added secured routes for changing password, fetching current user, updating user profile, and managing user avatar/cover image

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,6 +5,13 @@ import {
   userRegister,
   userLogout,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateUserProfile,
+  updateAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getUserWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "./../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -25,4 +32,16 @@ router.route("/login").post(userLogin);
 //secured routes
 router.route("/logout").post(verifyJWT, userLogout);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-user-profile").patch(verifyJWT, updateUserProfile);
+router.route("/user-avatar/update").patch(verifyJWT, upload, updateAvatar);
+router
+  .route("/user-cover-image/update")
+  .patch(verifyJWT, upload, updateUserCoverImage);
+router
+  .route("/user-channel-profile/:username")
+  .get(verifyJWT, getUserChannelProfile);
+router.route("/user-watch-history").get(verifyJWT, getUserWatchHistory);
+
 export default router;
